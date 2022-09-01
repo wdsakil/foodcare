ad20c2a5c9738e63bae39f968a1f520fac88e380
Sakil Ahmed <isakil2002@gmail.comgit config --global usergit config --global user.email isakil2002@gmail.com>
2022-09-01 22:12:31 +0600

    added all nav showhide code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
-const navBtn = document.querySelector('.fa-burger')
+//Navigation scripts
+const burger = document.querySelector('.fa-burger')
 const nav = document.querySelector('.nav')
+const backdrop = document.querySelector('.backdrop')
 
-navBtn.addEventListener('click', () => {
-    nav.classList.toggle('active')
+function deActivateNav() {
+    nav.classList.remove('active')
+    backdrop.classList.remove("active")
+}
+function activateNav() {
+    nav.classList.add('active')
+    backdrop.classList.add("active")
+}
+function deActivateNavIfActivated() {
+    if (nav.classList.contains('active')) {
+        deActivateNav()
+        return
+    }
+}
+
+burger.addEventListener('click', () => {
+    if (nav.classList.contains('active')) {
+        deActivateNav()
+        return
+    } else {
+        activateNav()
+    }
+})
+
+backdrop.addEventListener('click', () => {
+    deActivateNavIfActivated()
+
+})
+
+nav.addEventListener('click', (e) => {
+    if ((e.target.tagName == 'A') || (e.target.tagName == 'SPAN')) {
+        deActivateNavIfActivated()
+    }
 })
