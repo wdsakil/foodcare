be3bbd3ba0feca85c823647f7ab3b84bb77d3fe9
Sakil Ahmed <isakil2002@gmail.comgit config --global usergit config --global user.email isakil2002@gmail.com>
2022-09-02 17:44:22 +0600

    completed hero swiper

--- js/app.js
+++ js/app.js
@@ -39,3 +39,49 @@ nav.addEventListener('click', (e) => {
 window.addEventListener('scroll', ()=>{
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+// Hero Scripts
+
+let parallaxSliderOptions = {
+    // speed: 1000,
+    // autoplay: true,
+    // parallax: true,
+    // loop: true,
+    // pagination: {
+    //     el: '.hero .pagination',
+    //     dynamicBullets: true,
+    //     clickable: true
+    // },
+    // navigation: {
+    //     nextEl: '.slider-prlx .parallax-slider .next-ctrl',
+    //     prevEl: '.slider-prlx .parallax-slider .prev-ctrl'
+    // }
+
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+        el: ".hero .pagination",
+        clickable: true,
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+      
+}
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    speed: 300,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
